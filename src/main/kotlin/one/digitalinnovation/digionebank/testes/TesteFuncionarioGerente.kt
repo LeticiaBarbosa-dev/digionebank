89bfd1e28712c9e2411614c457f38a60a4d5f0de
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-06-29 12:16:05 -0300

    Criando testes e impressão de relatórios

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+import one.digitalinnovation.digionebank.Funcionario
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+    val pedro = Gerente("Pedro Barbosa", "126.126.126-09", 2000.0)
+
+    ImprimeRelatorioFuncionario.imprime(pedro)
+}
